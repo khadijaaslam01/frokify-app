1750b16fbcaafb30b80136bc65ed5461d6df8f08
khadija aslam <iamkadijahaslam0@gmail.com>
2023-08-03 01:55:23 +0500

    render recipe

---
+++ src/js/controller.js
@@ -0,0 +1,43 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    //render spinner
+    recipeView.renderSpinner();
+
+    //loading recipe
+
+    await model.loadRecipe(id);
+
+    //rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
